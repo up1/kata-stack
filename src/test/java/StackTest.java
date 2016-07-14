00fd58460e71cb9281d5d852da3836a44b5ed2c6
somkiat <somkiat.p@gmail.com>
2016-07-14 16:56:08 +0700

    Remove duplication code

--- src/test/java/StackTest.java
+++ src/test/java/StackTest.java
@@ -4,18 +4,20 @@ import static org.junit.Assert.assertEquals;
 
 public class StackTest {
 
+    StackString stack = new StackString();
+
     @Test public void
     เริ่มต้นสร้าง_Stack_ว่างๆขึ้นมา() {
-        StackString stack = new StackString();
         assertEquals(0, stack.size());
     }
 
     @Test public void
     ขนาดของ_Stack_จะเพิ่มขึ้นเมื่อทำการ_push_ข้อมูลเข้า() {
-        StackString stack = new StackString();
         stack.push("A");
         assertEquals(1, stack.size());
     }
+
+
 
 
 }
